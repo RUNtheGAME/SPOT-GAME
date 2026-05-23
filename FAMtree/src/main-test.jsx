6fe73e687657980fd7da03fa9dc387b759de99c8
RUNtheGAME <133536849+RUNtheGAME@users.noreply.github.com>
2026-05-23 17:52:15 +0300

    Delete FAMtree directory

--- FAMtree/src/main-test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppTestDesktop from './AppTestDesktop';
-import './index.css';
-import './index-test.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AppTestDesktop />);
-
